27d0c121d290d44aea45424107aec8559b475393
Noah Jennings <njenn001@odu.edu>
2020-04-16 14:11:31 -0400

    Applied A-Cubed Logo

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="./images/A-cubed.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to A<sup>3</sup>
         </p>
         <a
           className="App-link"
