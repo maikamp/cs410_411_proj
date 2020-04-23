db8bbed666267acdb6ea3912980b3a4f8aab97cb
njenning <njenning@cs.odu.edu>
2020-04-23 19:14:16 -0400

    nav/search bar

--- src/App.js
+++ src/App.js
@@ -5,18 +5,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <div class="topnav">
+        <a href="https://www.cs.odu.edu/~410crystal/">Home</a>
+        <a href="#news">Log In</a>
+        <a href="#contact">Sign Up</a>
+        <a href="#about">About</a> 
+        <b href =""> Search </b>
+        <input type="text" placeholder="..."></input>
+      </div>
+
       <header className="App-header">
-        <img src="./images/A-cubed.png" className="App-logo" alt="logo" />
+        <img src="./images/A-cubed.png" alt="logo" />
         <p>
           Welcome to A<sup>3</sup>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.cs.odu.edu/~410crystal/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Project Webpage
         </a>
       </header>
     </div>
