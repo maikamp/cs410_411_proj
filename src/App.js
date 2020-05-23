ef211dbc50e2a6d7f86971a92fa8216b23e831d7
njenning <njenning@cs.odu.edu>
2020-05-23 14:18:32 -0400

    React: restructure and components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import './styles/App.css';       
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <div className="App">
+    <Router> 
+      <Navbar> 
+        <br/> 
+          
+          <Route path="/" exact component={ExercisesList} />
+          <Route path="/edit/:id" component={EditExercise} />
+          <Route path="/create" component={CreateExercise} />
+          <Route path="/user" component={CreateUser} />
+      
+        </Navbar>
+    </Router>
 
-      <div class="topnav">
-        <a href="https://www.cs.odu.edu/~410crystal/">Home</a>
-        <a href="#news">Log In</a>
-        <a href="#contact">Sign Up</a>
-        <a href="#about">About</a> 
-        <b href =""> Search </b>
-        <input type="text" placeholder="..."></input>
-      </div>
-
-      <header className="App-header">
-        <img src="./images/A-cubed.png" alt="logo" />
-        <p>
-          Welcome to A<sup>3</sup>
-        </p>
-        <a
-          className="App-link"
-          href="https://www.cs.odu.edu/~410crystal/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Project Webpage
-        </a>
-      </header>
-    </div>
   );
 }
 
