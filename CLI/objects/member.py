88cbe959d9d074e8e74530221f1d6cfc7ca24561
njenning <njenning@cs.odu.edu>
2020-05-27 18:07:54 -0400

    Initial user class structure

---
+++ CLI/objects/member.py
@@ -0,0 +1,11 @@
+class Member(): 
+    def __init__(self):
+        
+        # Elements that faculty members have but guests do not
+        this.username = ""; 
+        this.email_address = ""; 
+        this.password = "";  
+        this.last_login = ""; 
+
+        # Extra elements to help with OOP 
+        this.credentials_path = "./config/cred.txt"; 
---
+++ CLI/objects/user.py
@@ -0,0 +1,9 @@
+class User(): 
+    def __init__(self):
+
+        # Elements that faculty members have but guests do not
+        this.id = "";
+        this.access_level = ""; 
+
+        # Extra elements to help with OOP 
+        this.output_path = './user/';
