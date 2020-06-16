d52590649990fa95438c01165b1b34678e538f74
jmur <jmurp030@odu.edu>
2020-06-16 14:24:55 -0400

    added artifact table.

--- src/server/acubed_db/setup.py
+++ src/server/acubed_db/setup.py
@@ -3,6 +3,25 @@ from database import cursor
 
 DATABASE_NAME = 'A3database'
 
+TABLES = {}
+
+TABLES['artifact'] = (
+    "CREATE TABLE 'Artifact' ("
+    " 'artifact_id' int() NOT NULL AUTO_INCREMENT, "
+    " 'owner_id' int() NOT NULL,"
+    " 'artifact_repo' int() NOT NULL,"
+    " 'artifact_last_changed' datetime,"
+    " 'artifact_access_level' int() NOT NULL,"
+    " 'artifact_name' varchar(40) NOT NULL,"
+    " 'artifact_original_source' text,"
+    " 'artifact_size' int(),"
+    " 'artifact_creation_date' datetime,"
+    " 'artifact_last_accessed' datetime,"
+    " 'artifact_access_count' int(),"
+    ") 
+)
+
 def create_database():
     cursor.execute("CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'utf8'".format(DATABASE_NAME))
-    
+    #convert to python f string or prepared query
+    #docker volumes persistant 
