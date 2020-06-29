5ebabf7b03efd596001114522baa777571bef8b8
njenning <njenning@cs.odu.edu>
2020-06-29 16:54:19 -0400

    Navbar Design

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,25 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
+import {  
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  Navbar, 
+  Form, 
+  FormControl, 
+  Button, 
+  Collapse, 
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  NavDropdown, 
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'react-bootstrap';
 
-import { BrowserRouter as Router, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
-import ExercisesList from "./exercises-list.component";
-import EditExercise from "./edit-exercise.component";
-import CreateExercise from "./create-exercise.component";
-import CreateUser from "./create-user.component";
+import "../styles/nav.css"
 
-export default class Navbar extends Component {
+export default class Navi extends Component {
 
   render() {
     return (
-      <Router> 
-        <Navbar> 
-          <br/>
-          <Route path="/edit/:id" component={EditExercise} />
-          <Route path="/create" component={CreateExercise} />
-          <Route path="/user" component={CreateUser} />
-
-        </Navbar>
-      </Router>
+      <Navbar className="color-nav" variant="dark">
+        <Navbar.Brand>A3</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto"> 
+            <Nav.Link href="#../../public/index.html">Home</Nav.Link>
+            <Nav.Link href="https://www.cs.odu.edu/~411crystal/home.html">Webpage</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <NavDropdown title="Account" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Sign Up</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Log In</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Settings</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Remove Account</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>  
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+       </Navbar.Collapse>
+      </Navbar>
     );
   }      
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 
-import Logo from "./components/logo-component"
+import Logo from "./components/logo-component"; 
+import Navi from "./components/navbar.component"; 
 import Footer from './components/footer-component';
 
 ReactDOM.render(<Logo/>, document.getElementById('header'));
+ReactDOM.render(<Navi/>, document.getElementById('navbar'));
 ReactDOM.render(<Footer/>, document.getElementById('footer'));
