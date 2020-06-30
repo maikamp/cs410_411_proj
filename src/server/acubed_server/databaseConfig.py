6fbe58aff8ed01895fb7264ed94d2852f7faa468
aaron berman <aberman@cs.odu.edu>
2020-06-30 16:56:09 -0400

    netowrking test fix

--- src/server/acubed_server/databaseConfig.py
+++ src/server/acubed_server/databaseConfig.py
@@ -3,7 +3,7 @@ import mysql.connector
 config = {
     'user': 'root',
     'password': 'rT1@4PlgTd',
-    'host': 'localhost:3306'
+    'host': 'acubed_db:3306'
 }
 
 db = mysql.connector.connect(**config)
