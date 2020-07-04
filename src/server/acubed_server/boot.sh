4ae61e9fe315fdaa6c9984228aa7f85de05ba326
aaron berman <aberman@cs.odu.edu>
2020-07-04 18:11:30 -0400

    better hack job

--- src/server/acubed_server/boot.sh
+++ src/server/acubed_server/boot.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-for i in 1 2 3
+for i in 1 2
 do
     python3 setup.py
-    echo Database is not ready, retrying in 5 secs...
-    sleep 5
+    echo Database is not ready, retrying in 30 secs...
+    sleep 30
 done
 flask run
