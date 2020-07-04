44e4f0009ca0f8c838f268546bd561382d3a2ffb
aaron berman <aberman@cs.odu.edu>
2020-07-04 17:37:33 -0400

    lets try flask and db setup together

---
+++ src/server/acubed_server/boot.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+while true: do
+    python3 setup.py
+    if [[ "$?" == "0"]]; then
+        break
+    fi
+    echo Database is not ready, retying in 5 secs...
+    sleep 5
+done
+flask run
