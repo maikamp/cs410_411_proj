8a6948d6c07186e5ca74da4e7dd217050fb51f98
aaron berman <aberman@cs.odu.edu>
2020-07-04 17:03:45 -0400

    two more errors as im learning

--- src/server/acubed_server/__init__.py
+++ src/server/acubed_server/__init__.py
@@ -1,8 +1,5 @@
 from flask import Flask
 from .config import Config
-from databaseConfig import connection
-from databaseConfig import cursor
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
