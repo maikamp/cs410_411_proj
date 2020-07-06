401a3ffaaa88a08b5aa50fbc18de44e27392354c
aaron berman <aberman@cs.odu.edu>
2020-07-06 18:32:48 -0400

    welcome to the level.... hopefullly.. maybe

--- src/server/acubed_svr/setup.py
+++ src/server/acubed_svr/setup.py
@@ -168,7 +168,7 @@ if __name__ == '__main__':
             val = [(1), (3), (5)]
             cursor.executemany(sqlinsert, val)
             print("Setting permission_level {}: ".format(str(val)), end='')
-            connection.connector.commit()
+            connection.commit()
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_NON_INSERTABLE_TABLE:
                 print("I messed up.")
