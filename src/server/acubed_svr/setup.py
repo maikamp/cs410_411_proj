98c5e9303bcc73b8034cabbb613cf2057b07b9b1
aberman <aberm003@odu.edu>
2020-07-06 10:44:27 -0400

    had to try and fix the permission level

--- src/server/acubed_svr/setup.py
+++ src/server/acubed_svr/setup.py
@@ -162,7 +162,7 @@ if __name__ == '__main__':
                 print("OK")
         try:
             permission_levels = "INSERT INTO permission_level (level) VALUES (%s)"
-            levels = [1 3 5]
+            levels = [1, 3, 5]
             print("Setting permission_level {}: ".format(levels), end='')
             cursor.executemany(permission_levels, levels)
             connection.commit()
