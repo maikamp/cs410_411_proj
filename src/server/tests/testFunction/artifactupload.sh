8ac1d42f7b7055f8e1862fd7645bc8d9fe98418b
aaron berman <aberman@cs.odu.edu>
2020-07-14 19:55:35 -0400

    test for artifact upload

--- src/server/tests/testFunction/artifactupload.sh
+++ src/server/tests/testFunction/artifactupload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#echo Test adduser 5: curl -X POST -d @testartifactupload1.json crystal.cpi.cs.odu.edu:5000/artifactupload
-#curl -X POST -d @testartifactupload1.json crystal.cpi.cs.odu.edu:5000/artifactupload
-#echo Test adduser default: curl -X POST -d @testartifactupload2.json crystal.cpi.cs.odu.edu:5000/artifactupload
-#curl -X POST -d @testartifactupload2.json crystal.cpi.cs.odu.edu:5000/artifactupload
+echo Test adduser 5: curl -X POST -d @testartifactupload1.json crystal.cpi.cs.odu.edu:5000/artifactupload
+curl -F 'filex=@testartifactupload1.json' -F '@filey=@adduser.sh' crystal.cpi.cs.odu.edu:5000/artifactupload
+echo Test adduser default: curl -X POST -d @testartifactupload2.json crystal.cpi.cs.odu.edu:5000/artifactupload
+curl -F 'filex=@testartifactupload2.json' -F '@filey=@register.sh' crystal.cpi.cs.odu.edu:5000/artifactupload
