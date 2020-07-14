eb8eb931e50a8c5f461996bb62eb47ab9c2d2b4e
stale_popcorn <mcamp004@odu.edu>
2020-07-14 16:40:48 -0400

    artifactupload.sh and test jsons

---
+++ src/server/tests/testFunction/artifactupload.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#echo Test adduser 5: curl -X POST -d @testartifactupload1.json crystal.cpi.cs.odu.edu:5000/artifactupload
+#curl -X POST -d @testartifactupload1.json crystal.cpi.cs.odu.edu:5000/artifactupload
+#echo Test adduser default: curl -X POST -d @testartifactupload2.json crystal.cpi.cs.odu.edu:5000/artifactupload
+#curl -X POST -d @testartifactupload2.json crystal.cpi.cs.odu.edu:5000/artifactupload
