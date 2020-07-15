69571f33f04ac9d60775eda79d39ac5b764901a2
aaron berman <aberman@cs.odu.edu>
2020-07-15 10:19:36 -0400

    eidt of the test 2 script

--- src/server/tests/test2.sh
+++ src/server/tests/test2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo Test 2: curl -X POST -d @test2.json crystal.cpi.cs.odu.edu:5000/login
-curl -X POST -d @test1.json crystal.cpi.cs.odu.edu:5000/login
+curl -X POST -d @test2.json crystal.cpi.cs.odu.edu:5000/login
