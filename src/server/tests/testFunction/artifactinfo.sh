33986b6629c3bc855819581b96bbb1d67bb57c0d
aaron berman <aberman@cs.odu.edu>
2020-07-20 17:12:38 -0400

    adding a second for get artifact info test

--- src/server/tests/testFunction/artifactinfo.sh
+++ src/server/tests/testFunction/artifactinfo.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo Test artifact info: curl -X POST -d @artifactInfo.json crystal.cpi.cs.odu.edu:5000/returnartifactinfo
 curl -X POST -d @artifactInfo.json crystal.cpi.cs.odu.edu:5000/returnartifactinfo
+echo Test artifact info 2: curl -X POST -d @artifactInfo2.json crystal.cpi.cs.odu.edu:5000/returnartifactinfo
+curl -X POST -d @artifactInfo2.json crystal.cpi.cs.odu.edu:5000/returnartifactinfo
