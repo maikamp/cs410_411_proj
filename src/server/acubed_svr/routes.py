b8b2f557cfef27e3359abb8365e4b8022ce0c799
aaron berman <aberman@cs.odu.edu>
2020-07-20 18:48:49 -0400

    s is for $tupid

--- src/server/acubed_svr/routes.py
+++ src/server/acubed_svr/routes.py
@@ -69,8 +69,8 @@ def register():
 
 @app.route('/artifactupload', methods = ['GET', 'POST'])
 def artifactupload():
-    #return db.artifactUpload(request.files.getlist('files[]'))
-    return db.artifactUpload(request.files['meta-data', 'file'])
+    return db.artifactUpload(request.files.getlist('file[]'))
+    #return db.artifactUpload(request.files['meta-data', 'file'])
 
 #@app.route('/adduser', methods = ['GET', 'POST'])
 #def adduser():
