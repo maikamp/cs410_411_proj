f072a02165115020b2254c0e8a58e4dee3413520
aaron berman <aberman@cs.odu.edu>
2020-07-20 17:32:29 -0400

    fixed apath to the upload

--- src/server/tests/testFunction/testactualupload.sh
+++ src/server/tests/testFunction/testactualupload.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-echo curl -F files=@testartifactupload1.json \ files=@../../simplemd.md crystal.cpi.cs.odu.edu:5000/artifactupload 
-#curl -X POST -d @testartifactupload1.json crystal.cpi.cs.odu.edu:5000/artifactupload
-curl -v -H "Content-Type:multipart/form-data" -F "meta-data=@testartifactupload1.json" -F "file=@../../simplemd.md" crystal.cpi.cs.odu.edu:5000/artifactupload
-#curl -v -H "Content-Type:multipart/form-data" -F "meta-data=@C:\Users\saurabh.sharma\Desktop\test.json;type=application/json" -F "file-data=@C:\Users\saurabh.sharma\Pictures\Saved Pictures\windows_70-wallpaper.jpg" http://localhost:7002/test/upload
+echo Test upload: curl -v -H "Content-Type:multipart/form-data" -F "meta-data=@testartifactupload1.json" -F "file=@../../acubed_svr/simplemd.md" crystal.cpi.cs.odu.edu:5000/artifactupload
+curl -v -H "Content-Type:multipart/form-data" -F "meta-data=@testartifactupload1.json" -F "file=@../../acubed_svr/simplemd.md" crystal.cpi.cs.odu.edu:5000/artifactupload
