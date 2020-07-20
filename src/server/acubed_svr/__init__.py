c0b11e74738311ae9f8f1b024f7217049d4e47f1
aaron berman <aberman@cs.odu.edu>
2020-07-20 11:18:12 -0400

    potential solution to the pandoc issue

--- src/server/acubed_svr/__init__.py
+++ src/server/acubed_svr/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from .config import Config
+import os
+os.environ.setdefault('PYPANDOC_PANDOC', '/usr/local/bin/pandoc')
 
 app = Flask(__name__)
 app.config.from_object(Config)
