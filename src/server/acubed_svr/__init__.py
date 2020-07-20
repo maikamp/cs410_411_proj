182576acaef850594bc0695e5d56b52cbde213ec
aaron berman <aberman@cs.odu.edu>
2020-07-20 16:47:05 -0400

    lets do moar with pandoc

--- src/server/acubed_svr/__init__.py
+++ src/server/acubed_svr/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from .config import Config
-import os
-os.environ.setdefault('PYPANDOC_PANDOC', '/usr/local/lib/python3.8/site-packages/pandoc')
+#import os
+#os.environ.setdefault('PYPANDOC_PANDOC', '/usr/local/lib/python3.8/site-packages/pandoc')
 
 app = Flask(__name__)
 app.config.from_object(Config)
