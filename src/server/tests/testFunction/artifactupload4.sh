d3eca7f36bf665fb7fc6bd8779c3eb9e8d2b5961
jmur <jmurp030@odu.edu>
2020-07-22 18:57:40 -0400

    yay peanutbutter

---
+++ src/server/tests/testFunction/artifactupload4.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo Test upload 4 BAD: curl -v -H "Content-Type:multipart/form-data" -F "meta-data=@testartifactupload4.json" -F "file=@Examplebad.gif" crystal.cpi.cs.odu.edu:5000/artifactupload -o ./curlresponse/artifactuploadresponse4.txt
+curl -v -H "Content-Type:multipart/form-data" -H "Content-Type: application/json" -F "meta-data=@testartifactupload4.json" -H "Content-Type: application/octet-stream" -F "file=@Examplebad.gif" crystal.cpi.cs.odu.edu:5000/artifactupload -o ./curlresponse/artifactuploadresponse4.txt
