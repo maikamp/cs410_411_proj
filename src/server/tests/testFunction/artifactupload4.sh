e557192104860aed341e53f9f520262e22ce968f
aaron berman <aberman@cs.odu.edu>
2020-07-31 11:05:31 -0400

    another artifact up load4 mimetype check

--- src/server/tests/testFunction/artifactupload4.sh
+++ src/server/tests/testFunction/artifactupload4.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo Test upload 4 BAD: curl -v -H "Content-Type:multipart/form-data" -F "meta-data=@testartifactupload4.json" -F "file=@Examplebad.gif" crystal.cpi.cs.odu.edu:5000/artifactupload -o ./curlresponse/artifactuploadresponse4.txt
-curl -v -H "Content-Type:multipart/form-data" -H "Content-Type: application/json" -F "meta-data=@testartifactupload4.json" -F "file=@Examplebad.gif" crystal.cpi.cs.odu.edu:5000/artifactupload -o ./curlresponse/artifactuploadresponse4.txt
+curl -v -H "Content-Type:multipart/form-data" -H "Content-Type: application/json" -F "meta-data=@testartifactupload4.json" -H "Content-Type: image/gif" -F "file=@Examplebad.gif" crystal.cpi.cs.odu.edu:5000/artifactupload -o ./curlresponse/artifactuploadresponse4.txt
