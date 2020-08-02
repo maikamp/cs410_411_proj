c30a5a9c6aaaf8eb458baffc78af877a970f1dcc
aaron berman <aberman@cs.odu.edu>
2020-08-02 16:09:27 -0400

    loginbad fix

--- src/server/tests/testFunction/adduser.sh
+++ src/server/tests/testFunction/adduser.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo Test adduser 5: curl -X POST -d @testadduser1.json crystal.cpi.cs.odu.edu:5000/adduser -o ./curlresponse/adduserresponse1.txt
-curl -X POST -d @testadduser1.json crystal.cpi.cs.odu.edu:5000/adduser -o ./curlresponse/adduserresponse1.txt
+curl -s -X POST -d @testadduser1.json crystal.cpi.cs.odu.edu:5000/adduser -o ./curlresponse/adduserresponse1.txt
 echo Test adduser default: curl -X POST -d @testadduser2.json crystal.cpi.cs.odu.edu:5000/adduser -o ./curlresponse/adduserresponse2.txt
-curl -X POST -d @testadduser2.json crystal.cpi.cs.odu.edu:5000/adduser -o ./curlresponse/adduserresponse2.txt
+curl -s -X POST -d @testadduser2.json crystal.cpi.cs.odu.edu:5000/adduser -o ./curlresponse/adduserresponse2.txt
 
--- src/server/tests/testFunction/loginbad.sh
+++ src/server/tests/testFunction/loginbad.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo Test bad login: curl -X POST -d @testlogin.json crystal.cpi.cs.odu.edu:5000/login -o ./curlresponse/loginresponse.txt
-curl -s -X POST -d @testlogin.json crystal.cpi.cs.odu.edu:5000/login -o ./curlresponse/loginresponse.txt
-cat ./curlresponse/loginresponse.txt |jq '.'
+echo Test bad login: curl -X POST -d @testloginbad.json crystal.cpi.cs.odu.edu:5000/login -o ./curlresponse/loginresponsebad.txt
+curl -s -X POST -d @testloginbad.json crystal.cpi.cs.odu.edu:5000/login -o ./curlresponse/loginresponsebad.txt
+cat ./curlresponse/loginresponsebad.txt |jq '.'
