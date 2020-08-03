37a726d0a14b6f3f355beccb873b563e48853e6b
jmur <jmurp030@odu.edu>
2020-08-03 13:23:02 -0400

    reverted

--- src/server/acubed_svr/database.py
+++ src/server/acubed_svr/database.py
@@ -818,8 +818,8 @@ class Database():
         self.cursor.execute(sql, data)
         temp = self.cursor.fetchall()
         artifact_change = temp[0][0]
-        #extracted_data = artifact_change.encode('utf-8')
-        readable_data = base64.decodebytes(artifact_change)
+        extracted_data = artifact_change.encode('utf-8')
+        readable_data = base64.decodebytes(extracted_data)
         
         if content.get("previous_version", "") == "":
             sql = "SELECT MAX(version) FROM artifact_change_record WHERE artifact_id = %s"
@@ -835,8 +835,8 @@ class Database():
         self.cursor.execute(sql, data)
         temp = self.cursor.fetchall()
         artifact_change_previous = temp[0][0]
-        #extracted_data_previous_version = artifact_change_previous.encode('utf-8')
-        readable_data_previous_version = base64.decodebytes(artifact_change_previous)
+        extracted_data_previous_version = artifact_change_previous.encode('utf-8')
+        readable_data_previous_version = base64.decodebytes(extracted_data_previous_version)
 
         d = difflib.HtmlDiff()
         return  (d.make_file(readable_data.split('\n'), readable_data_previous_version.split('\n')), 200)
