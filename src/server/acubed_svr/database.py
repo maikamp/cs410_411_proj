60da69721f0e363741ff0f1ba4ff34f8ddad5ae3
jmur <jmurp030@odu.edu>
2020-08-03 14:18:25 -0400

    comparing strings maybe

--- src/server/acubed_svr/database.py
+++ src/server/acubed_svr/database.py
@@ -821,8 +821,8 @@ class Database():
         print(type(artifact_change), file = sys.stderr)
         extracted_data = artifact_change.decode('utf-8')
         print(type(extracted_data), file = sys.stderr)
-        readable_data = base64.decodebytes(extracted_data)
-        print(type(readable_data), file = sys.stderr)
+        #readable_data = base64.decodebytes(extracted_data)
+        #print(type(readable_data), file = sys.stderr)
         
         if content.get("previous_version", "") == "":
             sql = "SELECT MAX(version) FROM artifact_change_record WHERE artifact_id = %s"
@@ -838,11 +838,11 @@ class Database():
         self.cursor.execute(sql, data)
         temp = self.cursor.fetchall()
         artifact_change_previous = temp[0][0]
-        #extracted_data_previous_version = artifact_change_previous.encode('utf-8')
+        extracted_data_previous_version = artifact_change_previous.decode('utf-8')
         #readable_data_previous_version = base64.decodebytes(extracted_data_previous_version)
 
         d = difflib.HtmlDiff()
-        return  (d.make_file(artifact_change.split('\n'), artifact_change_previous.split('\n')), 200)
+        return  (d.make_file(extracted_data.split('\n'), extracted_data_previous_version.split('\n')), 200)
         # read file into string, return said string
 
         #else:
