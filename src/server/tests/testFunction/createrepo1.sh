e0e2af4a9cf2aee5551a1a3eddc293a437e0a100
aaron berman <aberman@cs.odu.edu>
2020-08-04 16:39:44 -0400

    fix a cat on createreporesponse1

--- src/server/tests/testFunction/createrepo1.sh
+++ src/server/tests/testFunction/createrepo1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo Test create repository: curl -s -X POST -d @testcreaterepo1.json crystal.cpi.cs.odu.edu:5000/createrepo -o ./curlresponse/createreporesponse1.txt
 curl -s -X POST -d @testcreaterepo1.json crystal.cpi.cs.odu.edu:5000/createrepo -o ./curlresponse/createreporesponse1.txt
-cat ./curlresponse/createreporesponse1 | jq '.'
+cat ./curlresponse/createreporesponse1.txt | jq '.'
