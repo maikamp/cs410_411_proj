4d6097c75165e0c4add983dffcc32cf6f2beac1a
jmur <jmurp030@odu.edu>
2020-08-05 17:02:32 -0400

    Removed unused test

--- src/server/tests/testFunction/testUploadFunction.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo curl -X POST -d @testartifactupload1.json crystal.cpi.cs.odu.edu:5000/artifactupload
-curl -X POST -d @testartifactupload1.json crystal.cpi.cs.odu.edu:5000/artifactupload
